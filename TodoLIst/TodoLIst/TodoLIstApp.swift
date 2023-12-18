af6360c738db28765df48d36f4c2c2df0cb26e64
alysson912 <almenezes912@gmail.com>
2023-12-18 14:59:25 -0300

    Clean Project

---
+++ TodoLIst/TodoLIst/TodoLIstApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoLIstApp.swift
+//  TodoLIst
+//
+//  Created by Alysson Menezes on 18/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoLIstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
